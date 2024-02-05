20043206499c2c5784054939a1ca5e60c18a3d11
lulu38000 <156298153+lulu38000@users.noreply.github.com>
2024-02-05 16:56:13 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,17 @@ def mongraphique():
 @app.route("/histogramme/")
 def mongraphique2():
     return render_template("histogramme.html")
+  
+@app.route('/consultation/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
